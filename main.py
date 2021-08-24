13009d8162f24ba9131186323889a526c4baa990
André Hodapp <andre.hodapp@t-online.de>
2021-08-24 23:57:03 +0200

    looks good but one is faster than the other

--- main.py
+++ main.py
@@ -17,7 +17,31 @@ class Direction(Enum):
             return self.value-direction.value
         else:
             return -(direction.value-self.value)
+class Team:
+    red=0
+    blue=1
+def colorize(image, newColor):
+    """
+    Create a "colorized" copy of a surface (replaces RGB values with the given color, preserving the per-pixel alphas of
+    original).
+    :param image: Surface to create a colorized copy of
+    :param newColor: RGB color to use (original alpha values are preserved)
+    :return: New colorized Surface instance
+    """
+    image = image.copy()
 
+    # zero out RGB values
+    image.fill((0, 0, 0, 255), None, pygame.BLEND_RGBA_MULT)
+    # add in new RGB values
+    image.fill(newColor[0:3] + (0,), None, pygame.BLEND_RGBA_ADD)
+
+    return image
+def hexToColour( hash_colour ):
+    """Convert a HTML-hexadecimal colour string to an RGB triple-tuple"""
+    red   = int( hash_colour[1:3], 16 )
+    green = int( hash_colour[3:5], 16 )
+    blue  = int( hash_colour[5:7], 16 )
+    return ( red, green, blue )
 
 class Player:
     name="Player"
@@ -28,7 +52,8 @@ class Player:
     endurance= 10
     whichImageDirection=Direction
     whichImageExactly=0
-    def __init__(self, name, posX, posY, images, leben, endurance):
+    team=Team.red
+    def __init__(self, name, posX, posY, images, leben, endurance, team):
         self.name = name
         self.posX = posX
         self.posY = posY
@@ -36,7 +61,7 @@ class Player:
         self.leben = leben
         self.endurance = endurance
         self.whichImageDirection=Direction.Right
-
+        self.team = team
     def set_direction(self, direction):
         angle = self.whichImageDirection.get_diff(direction)
         print(angle, direction)
@@ -96,8 +121,19 @@ class Player:
             self.whichImageExactly +=1
             if(self.whichImageExactly>=len(self.images)):
                 self.whichImageExactly = 0
+    def getColor(self):
+        if(self.team==Team.red):
+            return '#4169E1' #'#0000FF'
+        if (self.team == Team.blue):
+            return '#FF0000'#'#A52A2A'
+        return '#000000'
     def draw(self, screen):
-        screen.blit(pygame.transform.rotate(self.images[self.whichImageExactly], self.whichImageDirection.value), (self.posX, self.posY))
+        image = pygame.transform.rotate(self.images[self.whichImageExactly], self.whichImageDirection.value)
+        imageBackground = pygame.transform.rotate(pygame.transform.scale(colorize(
+            self.images[self.whichImageExactly], hexToColour(self.getColor()))
+                                                 , (102,102)), self.whichImageDirection.value)
+        screen.blit(imageBackground, (self.posX, self.posY))
+        screen.blit(image, (self.posX, self.posY))
     #try it with this logic and it will work better and rotate automatically
 
 def quit_sequence(quit):
@@ -111,8 +147,8 @@ def running_loop(screen, mPL):
     movement = 2
     # RGB- RED, Green, Blue
     while not quit:
-        coordinateX = 0
-        coordinateY = 0
+        coordinateX = [0,0]
+        coordinateY = [0,0]
         for mouse in mousePlayerList:
             print("vor: ",mouse.posX, mouse.posY, mouse.name, mouse.endurance)
         screen.fill((128, 50, 0))
@@ -127,18 +163,42 @@ def running_loop(screen, mPL):
                     print("special")
         if keys[pygame.K_LEFT]:
             print("Left")
-            coordinateX-=movement
+            coordinateX[Team.red]-=movement
         if keys[pygame.K_RIGHT]:
             print("Right")
-            coordinateX+=movement
+            coordinateX[Team.red]+=movement
         if keys[pygame.K_UP]:
             print("Up")
-            coordinateY-=movement
+            coordinateY[Team.red]-=movement
         if keys[pygame.K_DOWN]:
             print("Down")
-            coordinateY+=movement
+            coordinateY[Team.red]+=movement
+        if keys[pygame.K_LEFT]:
+            print("Left")
+            coordinateX[Team.red] -= movement
+        if keys[pygame.K_RIGHT]:
+            print("Right")
+            coordinateX[Team.red] += movement
+        if keys[pygame.K_UP]:
+            print("Up")
+            coordinateY[Team.red] -= movement
+        if keys[pygame.K_DOWN]:
+            print("Down")
+            coordinateY[Team.red] += movement
+        if keys[pygame.K_a]:
+            print("a")
+            coordinateX[Team.blue] -= movement
+        if keys[pygame.K_d]:
+            print("d")
+            coordinateX[Team.blue] += movement
+        if keys[pygame.K_w]:
+            print("w")
+            coordinateY[Team.blue] -= movement
+        if keys[pygame.K_s]:
+            print("s")
+            coordinateY[Team.blue] += movement
         for mouse in mousePlayerList:
-            mouse.goTo(coordinateX, coordinateY)
+            mouse.goTo(coordinateX[mouse.team], coordinateY[mouse.team])
             mouse.draw(screen)
             mouse.gainendurance()
             print("nach: ", mouse.posX, mouse.posY, mouse.name, mouse.endurance)
@@ -157,7 +217,10 @@ pygame.display.set_caption("Mouse Race")
 mouseheadImage = pygame.image.load('mousehead/mousehead.png')
 pygame.display.set_icon(mouseheadImage)
 
-mousePlayerList = [Player("Maus1", 420, 480, load_images(), 3, 5)]
+mousePlayerList = [Player("Maus1", 120, 80, load_images(), 3, 5, Team.red),Player("Maus1", 120, 180, load_images(), 3, 5, Team.red),
+                    Player("Maus1", 120, 280, load_images(), 3, 5, Team.red),Player("Maus1", 120, 380, load_images(), 3, 5, Team.red),
+                   Player("Maus1", 120, 480, load_images(), 3, 5, Team.blue),Player("Maus1", 120, 580, load_images(), 3, 5, Team.blue),
+                   Player("Maus1", 120, 680, load_images(), 3, 5, Team.blue),Player("Maus1", 120, 780, load_images(), 3, 5, Team.blue)]
 running_loop(screen, mousePlayerList)
 
 
